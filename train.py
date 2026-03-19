cc5794b3e4a40cada59d522ee2d972b13558a4c9
mayukhdeb <mayukhmainak2000@gmail.com>
2026-03-19 09:31:26 -0400

    try out the fourier loss

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from grid_pattern_formation.models.rnn import RNN
 from grid_pattern_formation.models.trainer import Trainer
 from grid_pattern_formation.utils.seed import seed_everything
 from grid_pattern_formation.utils.config import load_config
-from topoloss import TopoLoss, LaplacianPyramid
+from topoloss import TopoLoss, LaplacianPyramid, PowerSpectrum
 from topoloss.scheduler import TauScheduler
 
 seed_everything(0)
@@ -35,24 +35,57 @@ trajectory_generator = TrajectoryGenerator(
 
 
 if options.topoloss_tau is not None:
+    # topo_loss = TopoLoss(
+    #     losses=[
+    #         LaplacianPyramid.from_layer(
+    #             model=model,
+    #             layer=model.RNN,
+    #             factor_h=9,
+    #             factor_w=9,
+    #             scale=options.topoloss_tau,
+    #             custom_weight_attribute_name="weight_hh_l0"
+    #         )
+    #     ],
+    #     strict_layer_type=False
+    # )
+
+    if options.topoloss_type == "laplacian_pyramid":
+        loss_config = LaplacianPyramid.from_layer(
+            model=model,
+            layer=model.RNN,
+            factor_h=9,
+            factor_w=9,
+            scale=options.topoloss_tau,
+            custom_weight_attribute_name="weight_hh_l0"
+        )
+    elif options.topoloss_type == "power_spectrum":
+        loss_config = PowerSpectrum.from_layer(
+            model=model,
+            layer=model.RNN,
+            freq_cutoff=5.0,
+            scale=options.topoloss_tau,
+            custom_weight_attribute_name="weight_hh_l0"
+        )
+    else:
+        raise ValueError(f"Unsupported topo loss type: {options.topoloss_type}")
+
     topo_loss = TopoLoss(
-        losses=[
-            LaplacianPyramid.from_layer(
-                model=model,
-                layer=model.RNN,
-                factor_h=9,
-                factor_w=9,
-                scale=options.topoloss_tau,
-                custom_weight_attribute_name="weight_hh_l0"
-            )
-        ],
+        losses=[loss_config],
         strict_layer_type=False
     )
+
     if options.topoloss_tau_scheduler == "linear_decay":
         tau_scheduler = TauScheduler(
             topo_loss=topo_loss,
             start_value=options.topoloss_tau,
             end_value=0.0,
+            num_steps=options.n_epochs * options.n_steps,
+        )
+    elif options.topoloss_tau_scheduler == "linear_warmup":
+        tau_scheduler = TauScheduler(
+            topo_loss=topo_loss,
+            start_value=0.0,
+            end_value=options.topoloss_tau,
             num_steps=options.n_epochs * options.n_steps,
         )
     else:
