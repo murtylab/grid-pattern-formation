49cd753cd4507ff234641baa9ce5c1920b5d5893
Kushal Dudipala <kushalreddyd@gmail.com>
2026-03-25 22:24:16 -0400

    topo param updates + new schedulers

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from grid_pattern_formation.models.rnn import RNN
 from grid_pattern_formation.models.trainer import Trainer
 from grid_pattern_formation.utils.seed import seed_everything
 from grid_pattern_formation.utils.config import load_config
-from topoloss import TopoLoss, LaplacianPyramid, PowerSpectrum
+from topoloss import TopoLoss, LaplacianPyramid
 from topoloss.scheduler import TauScheduler
 
 seed_everything(0)
@@ -51,19 +51,20 @@ if options.topoloss_tau is not None:
         loss_config = LaplacianPyramid.from_layer(
             model=model,
             layer=model.RNN,
-            factor_h=9,
-            factor_w=9,
+            factor_h=options.topoloss_factor_h,
+            factor_w=options.topoloss_factor_w,
             scale=options.topoloss_tau,
-            custom_weight_attribute_name="weight_hh_l0"
+            # custom_weight_attribute_name="weight_hh_l0"
         )
-    elif options.topoloss_type == "power_spectrum":
-        loss_config = PowerSpectrum.from_layer(
-            model=model,
-            layer=model.RNN,
-            freq_cutoff=5.0,
-            scale=options.topoloss_tau,
-            custom_weight_attribute_name="weight_hh_l0"
-        )
+        loss_config.custom_weight_attribute_name = "weight_hh_l0"
+    # elif options.topoloss_type == "power_spectrum":
+        # loss_config = PowerSpectrum.from_layer(
+        #     model=model,
+        #     layer=model.RNN,
+        #     freq_cutoff=5.0,
+        #     scale=options.topoloss_tau,
+        #     custom_weight_attribute_name="weight_hh_l0"
+        # )
     else:
         raise ValueError(f"Unsupported topo loss type: {options.topoloss_type}")
 
@@ -71,21 +72,38 @@ if options.topoloss_tau is not None:
         losses=[loss_config],
         strict_layer_type=False
     )
-
+    
     if options.topoloss_tau_scheduler == "linear_decay":
         tau_scheduler = TauScheduler(
             topo_loss=topo_loss,
             start_value=options.topoloss_tau,
             end_value=0.0,
-            num_steps=options.n_epochs * options.n_steps,
+            num_steps=options.n_epochs,
         )
-    elif options.topoloss_tau_scheduler == "linear_warmup":
+    elif options.topoloss_tau_scheduler == "linear_growth":
         tau_scheduler = TauScheduler(
             topo_loss=topo_loss,
             start_value=0.0,
             end_value=options.topoloss_tau,
-            num_steps=options.n_epochs * options.n_steps,
+            num_steps=options.n_epochs,
         )
+    elif options.topoloss_tau_scheduler == "cosine_growth":
+        tau_scheduler = TauScheduler(
+            topo_loss=topo_loss,
+            start_value=0.0,
+            end_value=options.topoloss_tau,
+            num_steps=options.n_epochs,
+            mode="cosine_growth"
+        )
+    elif options.topoloss_tau_scheduler == "cosine_decay":
+        tau_scheduler = TauScheduler(
+            topo_loss=topo_loss,
+            start_value=options.topoloss_tau,
+            end_value=0.0,
+            num_steps=options.n_epochs,
+            mode="cosine_decay"
+        )
+    
     else:
         tau_scheduler = None
 else:
