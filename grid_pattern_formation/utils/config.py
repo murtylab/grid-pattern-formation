346672a4d4a59251fb200936d43efcf2797ef370
Kushal Dudipala <kushalreddyd@gmail.com>
2026-03-29 11:23:17 -0400

    dont want defaults here

--- grid_pattern_formation/utils/config.py
+++ grid_pattern_formation/utils/config.py
@@ -3,16 +3,10 @@ import os
 import argparse
 import torch
 
-DEFAULTS = {
-    "sorscher_compatible": False,
-}
-
 def load_config(config_path: str):
     assert os.path.exists(config_path), f"Config file not found at {config_path}"
     with open(config_path) as f:
         cfg = yaml.safe_load(f)
-    for key, default in DEFAULTS.items():
-        cfg.setdefault(key, default)
     options = argparse.Namespace(**cfg)
     options.device = torch.device(options.device)
     options.dtype = getattr(torch, options.dtype)
